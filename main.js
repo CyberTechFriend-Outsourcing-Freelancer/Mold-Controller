95fdf002bb7de07c194c013540508fc2d035558b
0712023 <0712023@hanmail.net>
2021-01-14 16:05:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-var Flow = flow.get("flow") || {};
-var Bypass = flow.get("bypass") || {};
-var Purge = flow.get("purge") || {};
-const mode = flow.get("mode");
-if(JSON.stringify(Flow).length===0 || JSON.stringify(Bypass).length===0 || JSON.stringify(Purge).length===0){
-  return {"topic":"error","payload":"time setting error"};
-} else if(msg.payload === "" && mode===false){
-  return {"topic":"error","payload":"turn into Manual mode"};
-} else if(msg.payload !== "" && mode===true){
-  return {"topic":"error","payload":"turn into Auto mode"};
-}
+var Flow = {};
+var Bypass = {};
+var Purge = {};
+Flow["delay"] = flow.get("delay_1ch_flow") || 1
+Flow["runtime"] = flow.get("runtime_1ch_flow") || 1
+Bypass["delay"] = flow.get("delay_1ch_bypass") || 1
+Bypass["runtime"] = flow.get("runtime_1ch_bypass") || 1
+Purge["delay"] = flow.get("delay_1ch_purge") || 1
+Purge["runtime"] = flow.get("runtime_1ch_purge") || 1
+let mode = flow.get("1ch_mode");
+const SW_on = 0;
+const SW_off = 1;
 
 if(mode=="auto"){
   auto();
@@ -17,14 +18,14 @@ if(mode=="auto"){
 }
 
 function auto(){
-  setTimeout(()=>{
-    node.send({"topic":"flow", "payload":1});
-    setTimeout(()=>{
-      node.send({"topic":"flow", "payload":0});
-      setTimeout(()=>{
-        node.send({"topic":"bypass", "payload":1});
-        setTimeout(()=>{
-          node.send({"topic":"bypass", "payload":0});
+  var timer1 = setTimeout(()=>{
+    node.send({"topic":"1ch_flow", "payload":SW_on});
+    var timer2 = setTimeout(()=>{
+      node.send({"topic":"1ch_flow", "payload":SW_off});
+      var timer3 = setTimeout(()=>{
+        node.send({"topic":"1ch_bypass", "payload":SW_on});
+        var timer4 = setTimeout(()=>{
+          node.send({"topic":"1ch_bypass", "payload":SW_off});
         },Bypass.runtime*1000);
       },Bypass.delay*1000);
     },Flow.runtime*1000);
@@ -32,29 +33,42 @@ function auto(){
 }
 
 function manual(){
-  if(msg.payload == "flow"){
-    setTimeout(()=>{
-      node.send({"topic":"flow", "payload":1});
-      setTimeout(()=>{
-        node.send({"topic":"flow", "payload":0});
+  //Flow
+  if(msg.topic == "action_1ch_flow"){
+    var timer5 = setTimeout(()=>{
+      node.send({"topic":"1ch_flow", "payload":SW_on});
+      var timer6 = setTimeout(()=>{
+        node.send({"topic":"1ch_flow", "payload":SW_off});
       },Flow.runtime*1000);
     },Flow.delay*1000);
   }
 
-  if(msg.payload == "bypass"){
-    setTimeout(()=>{
-      node.send({"topic":"bypass", "payload":1});
-      setTimeout(()=>{
-        node.send({"topic":"bypass", "payload":0});
+  //Bypass
+  if(msg.topic == "action_1ch_bypass"){
+    var timer7 = setTimeout(()=>{
+      node.send({"topic":"1ch_bypass", "payload":SW_on});
+      var timer8 = setTimeout(()=>{
+        node.send({"topic":"1ch_bypass", "payload":SW_off});
       },Bypass.runtime*1000);
     },Bypass.delay*1000);
   }
 
-  if(msg.payload == "purge"){
-    setTimeout(()=>{
-      node.send({"topic":"purge", "payload":1});
-      setTimeout(()=>{
-        node.send({"topic":"purge", "payload":0});
+  //Purge
+  if(msg.topic == "action_1ch_purge"){
+    var timer9 = setTimeout(()=>{
+      node.send({"topic":"1ch_purge", "payload":SW_on});
+      var timer10 = setTimeout(()=>{
+        node.send({"topic":"1ch_purge", "payload":SW_off});
+      },Purge.runtime*1000);
+    },Purge.delay*1000);
+  }
+
+  //Reverse - Purge와 switch 반대
+  if(msg.topic == "action_1ch_reverse"){
+    var timer11 = setTimeout(()=>{
+      node.send({"topic":"1ch_purge", "payload":SW_off});
+      var timer12 = setTimeout(()=>{
+        node.send({"topic":"1ch_purge", "payload":SW_on});
       },Purge.runtime*1000);
     },Purge.delay*1000);
   }
